3c4f98fc312adc87464ff9693c2982d87e156a81
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-02-25 14:27:44 -0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React from 'react'
 import './App.css';
+import Home from './Components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
