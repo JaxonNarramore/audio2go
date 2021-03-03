faf210b38953227ca92e1c14234f1275e63f938d
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-03-03 10:33:28 -0600

    services working with no style

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -7,7 +7,10 @@ const data = [
         id: "1",
         key: "1",
         title: "MARINE",
-        text: "Text1.",
+        text: "Marine Audio Troubleshooting",
+        text2: "Marine Audio Installation",
+        text3: "Marine LED Lighting",
+        text4: "Appointment Fish Finder/ GPS installation",
         img: "https://static.wixstatic.com/media/e2cf28_c3d79131cb0949268f392b9022dcac21~mv2.jpg/v1/fill/w_478,h_340,al_c,lg_1,q_80/61Jv5KMxF5L__AC_SX425_.webp",
         img2: "https://static.wixstatic.com/media/e2cf28_1af6e7a2ec86495fa23b8d522be1ff39~mv2.png/v1/fill/w_478,h_340,al_c,q_85,usm_0.66_1.00_0.01/PCAM-Skeeter-Header.webp",
         img3: "https://static.wixstatic.com/media/e2cf28_565313368fc74eb2935cba6681dd02bc~mv2.jpg/v1/fill/w_478,h_340,al_c,lg_1,q_80/LedMarine.webp",
@@ -16,8 +19,11 @@ const data = [
     {
         id: "2",
         key: "2",
-        title: "AUTOMOTIVE",
-        text: "Text2.",
+        title2: "AUTOMOTIVE",
+        text: "Radio/Headunit Install",
+        text2: "Subwoofer & Amp Install",
+        text3: "4/5 Channel Amp Install",
+        text4: "Alarm / Remote Start Installation",
         img: "https://static.wixstatic.com/media/e2cf28_16d8cc2ae1d1424f8abe469cb44b8406~mv2.jpg/v1/fill/w_480,h_340,al_c,q_80,usm_0.66_1.00_0.01/kenwood%20deck.webp",
         img2: "https://static.wixstatic.com/media/e2cf28_da1b69800c7942c495206ad2685bc01b~mv2.gif",
         img3: "https://static.wixstatic.com/media/e2cf28_ac6bad3bc121429583bba5b0c1d1979d~mv2.jpg/v1/fill/w_480,h_340,al_c,q_80,usm_0.66_1.00_0.01/amps.webp",
@@ -26,8 +32,10 @@ const data = [
     {
         id: "3",
         key: "3",
-        title: "HOME/COMMERICAL",
-        text: "Text3.",
+        title3: "HOME/COMMERICAL",
+        text: "TV Mounting",
+        text2: "Audio Installation",
+        text3: "Security Camera Installation",
         img: "https://static.wixstatic.com/media/e2cf28_3eeba5f0205740d38af7ea4214942963~mv2.jpg/v1/fill/w_480,h_340,al_c,q_80,usm_0.66_1.00_0.01/IMG-0642_JPG.webp",
         img2: "https://static.wixstatic.com/media/e2cf28_5a78159648204f1f8385eed62933c693~mv2.jpg/v1/fill/w_480,h_340,al_c,q_80,usm_0.66_1.00_0.01/in%20ceiling%20speakers.webp",
         img3: "https://static.wixstatic.com/media/e2cf28_840a7aa62b6b4da0863c21f2161c7234~mv2.jpg/v1/fill/w_480,h_340,al_c,q_80,usm_0.66_1.00_0.01/security%2520camera_edited.webp"
@@ -38,33 +46,57 @@ export default function Services() {
     const [toggle, setToggle] = useState('1')
     return (
         <div>
-            {data.map(({ title, text, key, img, img2, img3, img4 }) => {
+            <p>Our Services</p>
+            {data.map(({ title, title2, title3, text, text2, text3, text4, key, img, img2, img3, img4 }) => {
                 return (
-                    <>
-                        <div className="main">
-                            <div className="text">
-                                <h1 onClick={() => setToggle(key)}>{title} </h1>
-                                {toggle === key ? (
-                                    <>
-                                        <p>{text}</p>
-                                    </>
-                                ) : null}
+                    <div className="main">
+                        <div className='titlez'>
+                            <div>
+                                <h2 className='title' onClick={() => setToggle(key)}>{title} </h2>
                             </div>
-
-                            <div className="img">
-                                {toggle === key ? (
-                                    <>
-                                        <img src={img} key={key} className="photo" />
-                                        <img src={img2} key={key} className="photo" />
-                                        <img src={img3} key={key} className="photo" />
-                                        <img src={img4} key={key} className="photo" />
-                                    </>
-                                ) : null}
+                            <div>
+                                <h2 className='title' onClick={() => setToggle(key)}>{title2} </h2>
+                            </div>
+                            <div>
+                                <h2 className='title' onClick={() => setToggle(key)}>{title3} </h2>
                             </div>
                         </div>
-                    </>
+                        {toggle === key ? (
+                            <div className='images'>
+                                <div>
+                                    <img src={img} key={key} className="photo" />
+                                    <p className='text'>{text}</p>
+                                </div>
+                                <div>
+                                    <img src={img2} key={key} className="photo" />
+                                    <p className='text'>{text2}</p>
+                                </div>
+                                <div>
+                                    <img src={img3} key={key} className="photo" />
+                                    <p className='text'>{text3}</p>
+                                </div>
+                                <div>
+                                    <img src={img4} key={key} className="photo" />
+                                    <p className='text'>{text4}</p>
+                                </div>
+                            </div>
+                        ) : null}
+                    </div>
                 );
             })}
         </div>
     )
 }
+
+
+
+{/* <div className="img">
+{toggle === key ? (
+    <>
+        <img src={img} key={key} className="photo" />
+        <img src={img2} key={key} className="photo" />
+        <img src={img3} key={key} className="photo" />
+        <img src={img4} key={key} className="photo" />
+    </>
+) : null}
+</div> */}
