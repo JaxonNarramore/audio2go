23ef9d98cb2c9348d9de13feb3c94658be14d79a
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-03-03 18:27:03 -0600

    carousel finished

--- src/Components/RecentInstalls.js
+++ src/Components/RecentInstalls.js
@@ -15,30 +15,60 @@ const items = [
 ]
 
 export default function RecentInstalls() {
+    const responsive = {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1024: {
+            items: 3
+        }
+    };
     return (
-        <div className='ri-container'>
-            <p>Recent Installs</p>
-            <div className='carousel'>
-                <AliceCarousel autoPlay autoPlayInterval="3000" infinite mouseTracking items={items} />
-            </div>
-        </div>
+        <div className='carousel'>
+            <AliceCarousel
+                duration={400}
+                autoPlay={true}
+                startIndex={1}
+                fadeOutAnimation={true}
+                mouseDragEnabled={true}
+                playButtonEnabled={true}
+                responsive={responsive}
+                autoPlayInterval={2000}
+                autoPlayDirection="rtl"
+                autoPlayActionDisabled={true}
+                mouseTracking
+                infinite
+                autoPlay
+                items={items}
+                paddingLeft={93}
+                paddingRight={30}>
+            </AliceCarousel>
+        </div >
     )
 }
 
 
 
+// const handleDragStart = (e) => e.preventDefault();
+
+// const items = [
+//     <img src='Images/1.jpeg' className="sliderimg" onDragStart={handleDragStart} />,
+//     <img src='Images/2.jpeg' className="sliderimg" onDragStart={handleDragStart} />,
+//     <img src='Images/3.jpeg' className="sliderimg" onDragStart={handleDragStart} />,
+//     <img src='Images/4.jpeg' className="sliderimg" onDragStart={handleDragStart} />,
+//     <img src='Images/5.jpeg' className="sliderimg" onDragStart={handleDragStart} />
+// ]
 
 // export default function RecentInstalls() {
 //     return (
 //         <div className='ri-container'>
 //             <p>Recent Installs</p>
-//             <AliceCarousel autoPlay autoPlayInterval="3000">
-//                 <img src='Images/1.jpeg' className="sliderimg" />
-//                 <img src='Images/2.jpeg' className="sliderimg" />
-//                 <img src='Images/3.jpeg' className="sliderimg" />
-//                 <img src='Images/4.jpeg' className="sliderimg" />
-//                 <img src='Images/5.jpeg' className="sliderimg" />
-//             </AliceCarousel>
+//             <div className='carousel'>
+//                 <AliceCarousel autoPlay autoPlayInterval="3000" infinite mouseTracking items={items} />
+//             </div>
 //         </div>
 //     )
 // }
