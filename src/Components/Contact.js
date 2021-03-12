d6ccd970a4a7a78f49a1e4ce229cfdb338ca3ad9
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-03-12 13:48:36 -0600

    ver close to what i want

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,19 +8,16 @@ export default function Contact() {
     return (
         <div>
             <div class="whole-contact">
-                <p className='ro-cuo'>Reach Out & Check Us Out</p>
-                <p className='ro-cuo-div'>Ask about our current packages, pricing and more. Our friendly, knowledgeable staff can answer all of your auto reconditioning questions.</p>
+                {/* <p className='ro-cuo'>Reach Out & Check Us Out</p>
+                <p className='ro-cuo-div'>Ask about our current packages, pricing and more. Our friendly, knowledgeable staff can answer all of your auto reconditioning questions.</p> */}
                 <div class="contact-container">
                     <div className='phone'>
-                        <p>Phone</p>
                         <p>512-588-2677</p>
                     </div>
                     <div className='email'>
-                        <p>Email</p>
                         <p>quotes@audio2goatx.com</p>
                     </div>
                     <div className='social'>
-                        <p>Socail Media</p>
                         <div className='icons'>
                             <a href="https://www.facebook.com/Audio2GoATX/" target='_blank'>
                                 <AiOutlineFacebook className='facebook' />
