32f2ff3d168551296c39f489900e1fdef40a9e11
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-03-17 09:18:28 -0500

    header change at customer request

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,13 +9,13 @@ export default function Header() {
                 <p className='leading'>The Leading Mobile Electronics Installation Specialists</p>
                 <div className='button-group'>
                     <div className='button-div'>
-                        <Link to='quotes' activeClass="active" spy={true} smooth={true} offset={-100} duration={100}>
-                            <button type="button" class="btn btn-outline-danger">Get A Quote</button>
+                        <Link to='services' activeClass="active" spy={true} smooth={true} offset={-160} duration={100}>
+                            <button type="button" class="btn btn-outline-light">Our Services</button>
                         </Link>
                     </div>
                     <div className='button-div'>
-                        <Link to='services' activeClass="active" spy={true} smooth={true} offset={-160} duration={100}>
-                            <button type="button" class="btn btn-outline-danger">Our Services</button>
+                        <Link to='quotes' activeClass="active" spy={true} smooth={true} offset={-100} duration={100}>
+                            <button type="button" class="btn btn-outline-light">Get A Quote</button>
                         </Link>
                     </div>
                 </div>
