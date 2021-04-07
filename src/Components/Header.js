8deb22944ff0028c469d426a966d3254aa56d5e7
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-04-07 07:11:04 -0500

    adding quote to homepage, good time to commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import './Header.css'
+import emailjs from 'emailjs-com';
+import './Quote.css'
+import Quote from './Quote'
 import { Link, animateScroll as scroll } from "react-scroll";
 
 export default function Header() {
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_jehhnpf', 'template_cfojvtk', e.target, 'user_JjjokygneXUjqq7WuOENs')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
+
     return (
         <div id='header' className='header-container'>
             <div className='a2g-container'>
                 <p className='leading'>The Leading Mobile Electronics Installation Specialists</p>
-                <div className='button-group'>
+                {/* <div className='button-group'>
                     <div className='button-div'>
                         <Link id='buttonid' to='services' activeClass="active" spy={true} smooth={true} offset={-100} duration={100}>
                             <button type="button" class="btn btn-outline-light">Our Services</button>
@@ -18,7 +33,8 @@ export default function Header() {
                             <button type="button" class="btn btn-outline-light">Get A Quote</button>
                         </Link>
                     </div>
-                </div>
+                </div> */}
+                <Quote />
             </div>
         </div>
     )
