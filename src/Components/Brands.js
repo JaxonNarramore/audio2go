65f1da73b44ad60ad4c680ccffc86dcfb01d74fb
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-06-28 16:54:49 -0500

    seo push

--- src/Components/Brands.js
+++ src/Components/Brands.js
@@ -23,13 +23,13 @@ export default function Brands() {
                         <img class="grid-image" src="https://static.wixstatic.com/media/fec686_b39c573e82334f3fb6cd19fdda068ecd~mv2.png/v1/fill/w_614,h_614,al_c,lg_1,q_90/fec686_b39c573e82334f3fb6cd19fdda068ecd~mv2.webp" alt="" />
                     </div>
                     <div class="grid-item">
+                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_7bb2bc490fda424bb79a435becdb23bd~mv2.png/v1/fill/w_780,h_520,al_c,lg_1,q_90/fec686_7bb2bc490fda424bb79a435becdb23bd~mv2.webp" alt="" />
+                    </div>
+                    <div class="grid-item">
                         <img class="grid-image" src="https://static.wixstatic.com/media/fec686_f6f241a83dca4b1b807317d8d4204269~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_f6f241a83dca4b1b807317d8d4204269~mv2.webp" alt="" />
                     </div>
                     <div class="grid-item">
                         <img class="grid-image" src="https://static.wixstatic.com/media/fec686_cebdbfe4f1004d1d877c4537f0b361eb~mv2.jpg/v1/fill/w_1600,h_1000,al_c,q_85/fec686_cebdbfe4f1004d1d877c4537f0b361eb~mv2.webp" alt="" />
-                    </div>
-                    <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_7bb2bc490fda424bb79a435becdb23bd~mv2.png/v1/fill/w_780,h_520,al_c,lg_1,q_90/fec686_7bb2bc490fda424bb79a435becdb23bd~mv2.webp" alt="" />
                     </div>
                     <div class="grid-item">
                         <img class="grid-image" src="https://static.wixstatic.com/media/fec686_ce2cbe601d7f49818a5f27d4d65f497b~mv2.jpg/v1/fill/w_576,h_432,al_c,lg_1,q_80/fec686_ce2cbe601d7f49818a5f27d4d65f497b~mv2.webp" alt="" />
@@ -38,10 +38,10 @@ export default function Brands() {
                         <img class="grid-image" src="https://static.wixstatic.com/media/fec686_0a07538b1acf4fd7a7181b1200511cc3~mv2.jpg/v1/fill/w_720,h_360,al_c,lg_1,q_80/fec686_0a07538b1acf4fd7a7181b1200511cc3~mv2.webp" alt="" />
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_087bc06d92044cf2be5a299f56d073d8~mv2.png/v1/fill/w_614,h_614,al_c,lg_1,q_90/fec686_087bc06d92044cf2be5a299f56d073d8~mv2.webp" alt="" />
+                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_773bf1d219ce4253b6fcbb41ba4fefbd~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_773bf1d219ce4253b6fcbb41ba4fefbd~mv2.webp" alt="" />
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_773bf1d219ce4253b6fcbb41ba4fefbd~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_773bf1d219ce4253b6fcbb41ba4fefbd~mv2.webp" alt="" />
+                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_087bc06d92044cf2be5a299f56d073d8~mv2.png/v1/fill/w_614,h_614,al_c,lg_1,q_90/fec686_087bc06d92044cf2be5a299f56d073d8~mv2.webp" alt="" />
                     </div>
                 </div>
             </div>
