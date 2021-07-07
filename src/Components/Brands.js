948cff7bbee1ea4de80e62883c2047fd7c4adce8
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-07-07 17:17:54 -0500

    outbound links

--- src/Components/Brands.js
+++ src/Components/Brands.js
@@ -8,40 +8,64 @@ export default function Brands() {
             <div className='bcc'>
                 <div class="brand-container">
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_691579a0f72544d7bc3c9cd5ad1579f1~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_691579a0f72544d7bc3c9cd5ad1579f1~mv2.webp" alt="Scosche" />
+                        <a href="https://www.scosche.com/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_691579a0f72544d7bc3c9cd5ad1579f1~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_691579a0f72544d7bc3c9cd5ad1579f1~mv2.webp" alt="Scosche" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_82c4641a04d04313ab40df562a735733~mv2.jpg/v1/fill/w_866,h_650,al_c,q_85/fec686_82c4641a04d04313ab40df562a735733~mv2.webp" alt="Pioneer" />
+                        <a href="https://www.pioneerelectronics.com/PUSA/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_82c4641a04d04313ab40df562a735733~mv2.jpg/v1/fill/w_866,h_650,al_c,q_85/fec686_82c4641a04d04313ab40df562a735733~mv2.webp" alt="Pioneer" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_bf8b9c2fa52448dd8187d6d0a2311cc2~mv2.gif" alt="Nav TV" />
+                        <a href="https://navtv.com/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_bf8b9c2fa52448dd8187d6d0a2311cc2~mv2.gif" alt="Nav TV" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image sony" src="https://static.wixstatic.com/media/fec686_94dccfc209184e0ebaf9171a939f88b9~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_94dccfc209184e0ebaf9171a939f88b9~mv2.webp" alt="Sony" />
+                        <a href="https://www.sony.com/en/">
+                            <img class="grid-image sony" src="https://static.wixstatic.com/media/fec686_94dccfc209184e0ebaf9171a939f88b9~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_94dccfc209184e0ebaf9171a939f88b9~mv2.webp" alt="Sony" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_f6f241a83dca4b1b807317d8d4204269~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_f6f241a83dca4b1b807317d8d4204269~mv2.webp" alt="Atrend" />
+                        <a href="https://atrendusa.com/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_f6f241a83dca4b1b807317d8d4204269~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_f6f241a83dca4b1b807317d8d4204269~mv2.webp" alt="Atrend" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_b39c573e82334f3fb6cd19fdda068ecd~mv2.png/v1/fill/w_614,h_614,al_c,lg_1,q_90/fec686_b39c573e82334f3fb6cd19fdda068ecd~mv2.webp" alt="Kenwood" />
+                        <a href="https://www.kenwood.com/usa/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_b39c573e82334f3fb6cd19fdda068ecd~mv2.png/v1/fill/w_614,h_614,al_c,lg_1,q_90/fec686_b39c573e82334f3fb6cd19fdda068ecd~mv2.webp" alt="Kenwood" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_ce2cbe601d7f49818a5f27d4d65f497b~mv2.jpg/v1/fill/w_576,h_432,al_c,lg_1,q_80/fec686_ce2cbe601d7f49818a5f27d4d65f497b~mv2.webp" alt="Kicker" />
+                        <a href="https://www.kicker.com/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_ce2cbe601d7f49818a5f27d4d65f497b~mv2.jpg/v1/fill/w_576,h_432,al_c,lg_1,q_80/fec686_ce2cbe601d7f49818a5f27d4d65f497b~mv2.webp" alt="Kicker" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_cebdbfe4f1004d1d877c4537f0b361eb~mv2.jpg/v1/fill/w_1600,h_1000,al_c,q_85/fec686_cebdbfe4f1004d1d877c4537f0b361eb~mv2.webp" alt="Polk" />
+                        <a href="https://www.polkaudio.com/en-us?gclid=CjwKCAjwoZWHBhBgEiwAiMN66cmcW7sSsZ8_0PUtTJdYBg12Fzg6kK87iwXmfwzVQ8SQ0Shu2j_AcRoCkAcQAvD_BwE">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_cebdbfe4f1004d1d877c4537f0b361eb~mv2.jpg/v1/fill/w_1600,h_1000,al_c,q_85/fec686_cebdbfe4f1004d1d877c4537f0b361eb~mv2.webp" alt="Polk" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_0a07538b1acf4fd7a7181b1200511cc3~mv2.jpg/v1/fill/w_720,h_360,al_c,lg_1,q_80/fec686_0a07538b1acf4fd7a7181b1200511cc3~mv2.webp" alt="Aamp" />
+                        <a href="https://aampglobal.com/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_0a07538b1acf4fd7a7181b1200511cc3~mv2.jpg/v1/fill/w_720,h_360,al_c,lg_1,q_80/fec686_0a07538b1acf4fd7a7181b1200511cc3~mv2.webp" alt="Aamp" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_7bb2bc490fda424bb79a435becdb23bd~mv2.png/v1/fill/w_780,h_520,al_c,lg_1,q_90/fec686_7bb2bc490fda424bb79a435becdb23bd~mv2.webp" alt="JBL" />
+                        <a href="https://www.jbl.com/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_7bb2bc490fda424bb79a435becdb23bd~mv2.png/v1/fill/w_780,h_520,al_c,lg_1,q_90/fec686_7bb2bc490fda424bb79a435becdb23bd~mv2.webp" alt="JBL" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_773bf1d219ce4253b6fcbb41ba4fefbd~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_773bf1d219ce4253b6fcbb41ba4fefbd~mv2.webp" alt="Memphis Car Audio" />
+                        <a href="https://www.memphiscaraudio.com/">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_773bf1d219ce4253b6fcbb41ba4fefbd~mv2.jpg/v1/fill/w_1022,h_1022,al_c,q_85,usm_0.66_1.00_0.01/fec686_773bf1d219ce4253b6fcbb41ba4fefbd~mv2.webp" alt="Memphis Car Audio" />
+                        </a>
                     </div>
                     <div class="grid-item">
-                        <img class="grid-image" src="https://static.wixstatic.com/media/fec686_087bc06d92044cf2be5a299f56d073d8~mv2.png/v1/fill/w_614,h_614,al_c,lg_1,q_90/fec686_087bc06d92044cf2be5a299f56d073d8~mv2.webp" alt="Axxera" />
+                        <a href="https://www.axxeraaudio.com/firmware">
+                            <img class="grid-image" src="https://static.wixstatic.com/media/fec686_087bc06d92044cf2be5a299f56d073d8~mv2.png/v1/fill/w_614,h_614,al_c,lg_1,q_90/fec686_087bc06d92044cf2be5a299f56d073d8~mv2.webp" alt="Axxera" />
+                        </a>
                     </div>
                 </div>
             </div>
